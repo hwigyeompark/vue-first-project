11937386e47fda6a8e88587d1ff51d30497135b9
hwigyeompark <phg5590@gmail.com>
2018-12-07 09:20:45 +0900

    update show modal

--- src/components/DetailPage.vue
+++ src/components/DetailPage.vue
@@ -30,22 +30,22 @@
       </div>
     </div>
     <!--show join popup-->
-    <div class="modal fade" id="join-induce-modal-wrap">
-        <div class="modal-content join-induce-modal">
-          <div class="modal-header">
-            <h3 class="modal-title">회원가입</h3>
-          </div>
-          <div class="modal-body">
-            <div class="join-induce-box">
-              <div class="join-induce-ment">
-                <p>지금 가입하면 꿈꾸던 기업에 재직 중인 현직자와</p>
-                <p><span>익명</span>으로 대화할 수 있습니다.</p>
-              </div>
-              <div class="join-btn-wrap">
-                <button class="facebook-btn">페이스북 계정으로 회원가입</button>
-                <button class="google-btn">구글 계정으로 회원가입</button>
-                <button class="naver-btn">네이버 계정으로 회원가입</button>
-              </div>
+    <div class="modal" id="join-induce-modal-wrap">
+      <div class="modal-content join-induce-modal">
+        <div class="modal-header">
+          <h3 class="modal-title">회원가입</h3>
+        </div>
+        <div class="modal-body">
+          <div class="join-induce-box">
+            <div class="join-induce-ment">
+              <p>지금 가입하면 꿈꾸던 기업에 재직 중인 현직자와</p>
+              <p><span>익명</span>으로 대화할 수 있습니다.</p>
+            </div>
+            <div class="join-btn-wrap">
+              <button class="facebook-btn">페이스북 계정으로 회원가입</button>
+              <button class="google-btn">구글 계정으로 회원가입</button>
+              <button class="naver-btn">네이버 계정으로 회원가입</button>
+            </div>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn" data-dismiss="modal" @click="showJoinFooterPopup">나중에 하기</button>
@@ -54,67 +54,69 @@
       </div>
     </div>
     <!--show join footer popup-->
-    <div class="modal fade" id="join-induce-modal-footer-wrap">
-        <div class="modal-content join-induce-footer-modal">
-          <div class="modal-body">
-            <div class="join-induce-footer-box">
-              <div class="join-induce-footer-ment">
-                <p>지금 가입하면 꿈꾸던 기업에 재직 중인 현직자와 <span class="red-span">익명</span>으로 대화할 수 있습니다.</p>
-              </div>
-              <div class="join-btn-footer-wrap">
-                <button class="sns-join-btn">SNS계정으로 빠른 회원가입</button>
-                <p>또는</p>
-                <button class="login-btn">로그인</button>
-              </div>
+    <div class="modal" id="join-induce-modal-footer-wrap">
+      <div class="modal-content join-induce-footer-modal">
+        <div class="modal-body">
+          <div class="join-induce-footer-box">
+            <div class="join-induce-footer-ment">
+              <p>지금 가입하면 꿈꾸던 기업에 재직 중인 현직자와 <span class="red-span">익명</span>으로 대화할 수 있습니다.</p>
+            </div>
+            <div class="join-btn-footer-wrap">
+              <button class="sns-join-btn">SNS계정으로 빠른 회원가입</button>
+              <button class="or">또는</button>
+              <button class="login-btn">로그인</button>
             </div>
           </div>
         </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  import Header from './Header'
-  import {eventBus} from '../main'
+import Header from './Header'
+import {eventBus} from '../main'
 
-  export default {
-    name: 'detailPage',
-    components: {Header},
-    created(){
-      this.showJoinPopup()
-      eventBus.$on('get-detail', sendDetailList => {
-        this.receivedDetailList = sendDetailList
-      })
+export default {
+  name: 'detailPage',
+  components: {Header},
+  mounted () {
+    this.showJoinPopup()
+    eventBus.$on('get-detail', sendDetailList => {
+      this.receivedDetailList = sendDetailList
+    })
+  },
+  data () {
+    return {
+      receivedDetailList: [],
+      selectedDetailNum: ''
+    }
+  },
+  methods: {
+    showJoinPopup: function () {
+      console.log('success show join popup')
+      $('#join-induce-modal-wrap').modal('show').fadeIn
     },
-    data(){
-      return{
-        receivedDetailList: [],
-        selectedDetailNum: ''
-      }
-    },
-    methods: {
-      showJoinPopup: function () {
-        console.log('success show join popup')
-        $('#join-induce-modal-wrap').appendTo('body').modal('show')
-      },
-      showJoinFooterPopup: function () {
-        console.log('success show join footer popup')
-        $('#join-induce-modal-footer-wrap').appendTo('body').modal('show')
-      }
+    showJoinFooterPopup: function () {
+      $('#join-induce-modal-footer-wrap').modal('show').fadeIn
     }
   }
+}
 </script>
 
 <style lang="less" scoped>
-  .detail-wrap{
+  .detail-wrap {
     margin: 4%;
   }
-  .detail-box-wrap{
+
+  .detail-box-wrap {
     .detail-wrap
   }
-  .form-group{
+
+  .form-group {
     border-bottom: 1px solid #e0e0e0;
   }
+
   .join-induce-box {
     width: 100%;
     margin: 0 auto;
@@ -131,7 +133,8 @@
       vertical-align: center;
     }
   }
-  .red-span{
+
+  .red-span {
     color: red;
   }
 
@@ -143,7 +146,7 @@
       padding: 10px;
       color: #ffffff;
       border: none;
-      &:hover{
+      &:hover {
         cursor: pointer;
       }
     }
@@ -158,26 +161,35 @@
     }
   }
 
-  .join-btn-footer-wrap{
+  .join-btn-footer-wrap {
     display: flex;
     justify-content: center;
-    & button{
-      padding: 3%;
+    & button {
+      padding: 1%;
       border: none;
       border: 1px solid #555555;
-      margin: 1%;
+      margin: 0.5%;
     }
-    & .sns-join-btn{
+    & .sns-join-btn {
       background-color: #00c854;
       color: #ffffff;
     }
-    & .login-btn{
+    & .or{
+      background: transparent;
+      border: none;
+    }
+    & .login-btn {
       background-color: #ffffff;
       color: #000000;
     }
   }
+  .join-induce-footer-ment{
+    display: flex;
+    justify-content: center;
+  }
+
   @media screen and (max-width: 480px) {
-    .join-induce-modal{
+    .join-induce-modal {
     }
   }
 </style>
